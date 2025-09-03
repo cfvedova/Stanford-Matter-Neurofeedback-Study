87cf095ba7c2a9ddef527eed06c3c23b55baa78e
assuntaciarlo <79250141+assuntaciarlo@users.noreply.github.com>
2025-09-03 19:55:11 +0200

    Create 2_create_sdm_from_prt.py

---
+++ fmri/2_offline_analysis/2_create_sdm_from_prt.py
@@ -0,0 +1,9 @@
+'''
+
+Create SDM files from PRT files
+
+This script will create design matrix files (SDM) for BrainVoyager offline GLM computation, starting from PRT files.
+
+Currently, this step is performed via BV GUI.
+
+'''
